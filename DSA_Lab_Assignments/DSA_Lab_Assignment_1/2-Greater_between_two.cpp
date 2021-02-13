d61ba570423390993df217cd0fe09eef168db2ab
Nandkishor Nrr <71880979+NandkishorNrr@users.noreply.github.com>
2021-02-13 20:29:33 +0530

    Greater between 2 numbers.
    2. WAP to find greater between 2 numbers

---
+++ DSA_Lab_Assignments/DSA_Lab_Assignment_1/2-Greater_between_two.cpp
@@ -0,0 +1,16 @@
+// 2. WAP to find greater between 2 numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<"Enter two numbers.\n";
+    cin>>a>>b;
+    if(a==b)
+    cout<<a<<" = "<<b;
+    if(a>b)
+    cout<<a<<" > "<<b;
+    if(a<b)
+    cout<<b<<" > "<<a;
+    return 0;
+}
